b623d8aa274e04eabf8ee998581d8259f53302b0
Dylan <dylanjohnson4070@gmail.com>
2024-04-19 00:07:02 -0500

    Dataset combining is almost done, works with full data, partial data exporting is still not working.

--- main.py
+++ main.py
@@ -1,32 +1,26 @@
+import os.path
+
+# DO NOT MOVE THIS NEXT LINE OF CODE, IT HAS TO RUN WITH THE IMPORT STATMENTS, THE CODE WILL NOT WORK
 import torch
+import combineTrain
+import combine_data
 from ultralytics import YOLO
-import os
 
 torch.compile()
-
-
 # as of right now the dataset will not work directily from the project directory, it needs to be in the default
 # for ultralytics
 # --fixed
 
 # tr = input('Train Model Y/n')
-def runTrain():
-    if torch.cuda.is_available():
-        model = YOLO('yolov8s-seg.pt')
-        results = model.train(data=os.path.abspath('./Datasets/yolov8/IDREC-3.v1i.yolov8/data.yaml'),
-                              device=0, patience=30, cls=0.7)
-
-    else:
-        model = YOLO('yolov8s-seg.pt')
-        if input("Non-CUDA compatible GPU detected... proceed with CPU? y/n").lower() == 'y':
-            results = model.train(data=os.path.abspath('./Datasets/yolov8/IDREC-3.v1i.yolov8/data.yaml'),
-                                  patience=30, plots=True, cls=.7)
-        else:
-            results = ("Failed to train, either run with CPU or check to see if you are using CONDA3.10 with PyTorch "
-                       "installed")
-
-    return results
 
 
 if __name__ == '__main__':
-    rs = runTrain()
+    # combineTrian.addData('./Working_Models/yolov8segment/best.pt',
+    #                   './Datasets/yolov8seg/IDREC-3-ONESHOTS 2.v2i.yolov8/data.yaml')
+    # combineTrian.newModel()
+    #combineTrain.combineTrain('CombinedDatasets/2024-04-15 07039', 'Working_Models/yolov8segment/best.pt')
+    # combineTrain.createTrainingSet('Working_Models/yolov8segment/best.pt', 'Datasets/yolov8seg/IDREC-3-ONESHOTS 2.v2i.yolov8')
+    #print(len(dict(YOLO('Working_Models/yolov8segment/best.pt').named_modules())))
+    model = YOLO('yolov8n-seg.pt')
+    model.train(epochs=40, warmup_epochs=0, pretrained='./Working_Models/yolov8segment/best.pt', freeze=22,
+                data=os.path.abspath('Cool/11470343422024-04-17/data.yaml'))
