9c435c7657bbf8ce4997ad4ec529b2f5a60230ef
William Woodson <wjwoodson03@gmail.com>
2024-04-28 16:29:26 -0500

    Refactored the Gui files

--- Gui/Initial Window/mainwindow.py
+++ Gui/Initial Window/mainwindow.py
@@ -3,14 +3,13 @@ import sys
 import time
 from os import system
 from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import Signal, Slot
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
 #     pyside6-uic form.ui -o ui_form.py, or
 #     pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_MainWindow
-
-
 
 
 class MainWindow(QMainWindow):
