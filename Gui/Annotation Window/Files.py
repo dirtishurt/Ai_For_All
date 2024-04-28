2aa50578f799e566dca9814377b1ef35ac32f7ec
Dylan <dylanjohnson4070@gmail.com>
2024-04-28 16:30:05 -0500

    Started Work on annotation gui

--- Gui/Annotation Window/Files.py
+++ Gui/Annotation Window/Files.py
@@ -1,11 +1,14 @@
-from PySide6.QtWidgets import QWidget, QLabel, QApplication, QListWidget, QWidgetAction, QFileDialog
+from PySide6.QtWidgets import QWidget, QLabel, QApplication, QListWidget, QWidgetAction, QFileDialog, QVBoxLayout
 from PySide6.QtCore import QThread, Qt, Signal, Slot
 from PySide6.QtGui import QImage, QPixmap, QColor
+
 
 class Files(QListWidget):
     def __init__(self, window):
         super().__init__(window)
-
-
-
-
+        self.label = QLabel(window)
+        self.th = None
+        self.title = 'Files'
+        self.layout = QVBoxLayout()
+        self.setAcceptDrops(True)
+        self.setSortingEnabled(True)
